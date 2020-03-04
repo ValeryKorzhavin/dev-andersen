21f786d80ff6cde44a013da0066da48a69cb9576
Valery Korzhavin <white.and.bushy@gmail.com>
2020-03-04 11:48:00 +0300

    use logger

--- class-loading-task/src/com/company/PrintService.java
+++ class-loading-task/src/com/company/PrintService.java
@@ -1,15 +1,19 @@
 package com.company;
 
+import java.util.logging.Logger;
+
 public class PrintService {
 
     private MessageProvider messageProvider;
+
+    private static final Logger LOGGER = Logger.getLogger(PrintService.class.getName());  
     
     public void setMessageProvider(MessageProvider messageProvider) {
         this.messageProvider = messageProvider;
     }
 
     public void printMessage() {
-        System.out.println(messageProvider.getMessage());
+        LOGGER.info(messageProvider.getMessage());
     }
 
 }
