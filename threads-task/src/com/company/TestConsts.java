e872c6970a337c88185590038b929ae1531dd76c
Valery Korzhavin <white.and.bushy@gmail.com>
2020-03-10 11:38:59 +0300

    initial commit

---
+++ threads-task/src/com/company/TestConsts.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Не менять.
+ */
+public class TestConsts {
+    private TestConsts() {
+        // No-op.
+    }
+
+    public static final int MAX_THREADS = 4;
+    public static final int N = 10;
+}
